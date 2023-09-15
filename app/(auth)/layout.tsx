da948f4fc244d81f45d9665a5ca68644c3d55d33
Lucky <goyallucky2020@gmail.com>
2023-09-15 11:37:38 +0530

    upload nextjs basic structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const metaData = {
+  title: "Threads",
+  description:
+    "A full stack clone of threads application based on NextJs 13 and Mern stack",
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <h1>Hello world</h1>;
+}
